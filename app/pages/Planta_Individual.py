41899ce4394298591573adf12d1045fd37e6181e
DanielBessadev <danielbessadev@gmail.com>
2023-11-15 13:45:09 -0300

    first2

--- app/pages/Planta_Individual.py
+++ app/pages/Planta_Individual.py
@@ -1,11 +1,5 @@
 from datetime import datetime, date
 
-from functions.functions import all_reports_by_stock
-from functions.graphs import graph_compare_report
-from functions.report import report_columns
-from functions.setups import frequency, setups_info
-from functions.tickers import tickers
-
 import streamlit as st
 
 # Streamlit
