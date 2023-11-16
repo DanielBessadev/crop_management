32a44815a2bc0c72dbb5a60e57ff27fc2ff39579
DanielBessadev <danielbessadev@gmail.com>
2023-11-16 13:25:44 -0300

    ind4

--- app/pages/Planta_Individual.py
+++ app/pages/Planta_Individual.py
@@ -10,20 +10,20 @@ with c1:
     name = st.selectbox('Planta', names)
     selected_species = species.loc[species.name == name]
 with c2:
-    st.write("(", selected_species['species'].values,")")
+    st.write("(", selected_species['species'],")")
 with c3:
     st.write("Image")
     # Image columns #st.image
 
 c1, c2, c3 = st.columns(3)
 with c1:
-    st.write(selected_species['popular_names'].values)
+    st.write(selected_species['popular_names'])
 with c2:
     selected_specimens = specimens.loc[specimens.id_species == int(selected_species.index.values)]
     gps = st.selectbox('Localização', selected_specimens['gps_x'].sort_values().to_list())
     selected_specimen = selected_specimens.loc[selected_specimens.gps_x == gps]
 with c3:
     # Age calculation
-    age = selected_specimen['date_plantation'].values
+    age = selected_specimen['date_plantation']
     st.write("Idade:", age)
 
