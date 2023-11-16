563e27087fee102604aa7cff8e661c555b7df337
DanielBessadev <danielbessadev@gmail.com>
2023-11-16 14:12:32 -0300

    ind5

--- app/functions/functions.py
+++ app/functions/functions.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-pd.set_option('display.max_colwidth', 100)
+pd.set_option('display.max_colwidth', 1000)
 
 # Dataframes
 species = pd.read_excel('database/roca_andiroba.xlsx', sheet_name='Species', index_col='id_species')
