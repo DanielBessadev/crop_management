f95f633b04d8f0de39dc8f715d07126c4174dbc0
DanielBessadev <danielbessadev@gmail.com>
2023-11-16 11:47:08 -0300

    individual

---
+++ app/functions/functions.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+# Dataframes
+species = pd.read_excel(io='/app/crop_management/database/roca_andiroba.xlsx', sheet_name='Species', index_col='id_species')
+names = species['name'].sort_values().to_list()
+
+specimens = pd.read_excel(io='/app/crop_management/database/roca_andiroba.xlsx', sheet_name='Specimen', index_col='id_specimen')
+
+# Retornar somente uma planta
--- app/pages/Planta_Individual.py
+++ app/pages/Planta_Individual.py
@@ -1,8 +1,29 @@
-from datetime import datetime, date
-
 import streamlit as st
+
+from functions.functions import species, specimens, names
 
 # Streamlit
 st.set_page_config(page_title='Meu Setup', layout='wide')
 
-st.subheader("Performance de setups por Ativo")
+c1, c2, c3 = st.columns(3)
+with c1:
+    name = st.selectbox('Planta', names)
+    selected_species = species.loc[species.name == name]
+with c2:
+    st.write("(", selected_species['spieces'].values,")")
+with c3:
+    # Image columns
+    #st.image
+
+c1, c2, c3 = st.columns(3)
+with c1:
+    st.write(selected_species['popular_names'].values)
+with c2:
+    selected_specimens = specimens.loc[specimens.id_species == int(selected_species.index.values)]
+    gps = st.selectbox('Localização', selected_specimens['gps_x'].sort_values().to_list())
+    selected_specimen = selected_specimens.loc[selected_specimens.gps_x == gps]
+with c3:
+    # Age calculation
+    age = selected_specimen['date_plantation'].values
+    st.write("Idade:", age)
+
