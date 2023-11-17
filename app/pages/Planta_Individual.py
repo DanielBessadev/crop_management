90b9b2a1c958c8dded25561ca8160ed12dfd6064
DanielBessadev <danielbessadev@gmail.com>
2023-11-17 15:20:03 -0300

    ind7

--- app/pages/Planta_Individual.py
+++ app/pages/Planta_Individual.py
@@ -48,9 +48,6 @@ with c1:
 with c2:
     st.write("Image")
 
-df = pd.DataFrame(data=[[float(selected_specimen.gps_x.to_string(index=False, header=False))], 
-                         [float(selected_specimen.gps_y.to_string(index=False, header=False))],
-                         [float(10)],
-                         [18]],
+df = pd.DataFrame(data=[[float(selected_specimen.gps_x.to_string(index=False, header=False)), float(selected_specimen.gps_y.to_string(index=False, header=False))]],
     columns=['lat', 'lon'])
 st.map(df)
